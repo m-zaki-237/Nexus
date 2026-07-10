13fe00934820ddc70c2a0f9e8ef3c1746a6aaa69
m-zaki-237 <muhammadzakriaawan237@gmail.com>
2026-07-10 17:00:59 +0500

    cookies fixed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -26,12 +26,12 @@ export const registerUser = async (req, res) => {
       expiresIn: "7d",
     });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.status(201).json({ message: "User registered successfully", token });
   } catch (error) {
@@ -60,12 +60,11 @@ export const loginUser = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
     res.status(200).json({ message: "User logged in successfully", token });
   } catch (error) {
     console.error(error);
